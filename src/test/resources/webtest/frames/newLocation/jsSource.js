7e63e76c009101a82cf404000e795bb758832b1b
Rafal Kordalski <rafal.kordalski@nask.pl>
2013-04-23 12:46:38 +0200

    test fix

--- src/test/resources/webtest/frames/newLocation/jsSource.js
+++ src/test/resources/webtest/frames/newLocation/jsSource.js
@@ -1,6 +1,4 @@
 function fire()
 {
-	document.write('<div width="10" heigth="10">');
-	document.write('<p><a href="http://www.apache.org/">link1</a></p>');
-	document.write('</div>');
+	document.write('<div width="10" heigth="10" />');
 }
